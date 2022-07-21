8db1d8bed0ec9443a2148eef55e333c0b18e8b31
irisrumtub <tvburis@gmail.com>
2022-07-21 19:36:28 +0300

    Update toJadenCase.js

--- codewars/toJadenCase.js
+++ codewars/toJadenCase.js
@@ -1,10 +1,10 @@
-x = {"key":"value"}
+st = "A quick brown fox did something"
 
-if (typeof x == "object"){
-    if (x.key == "value"){
-        return true
+String.prototype.toJadenCase = function () {
+    let x = this.split(" ")
+    for (let i=0;i<=x.length-1; i++) {
+        x[i]= x[i].charAt(0).toUpperCase() + x[i].substring(1)
     }
-    else return false
-
-    
-}
+    x = x.join(" ")
+    return x
+  };
